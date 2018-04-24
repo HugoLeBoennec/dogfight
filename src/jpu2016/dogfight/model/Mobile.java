9c415136a8ff6b830d147b0179a114f5e0b55fb4
julienliguori <33621048+julienliguori@users.noreply.github.com>
2018-04-24 11:24:16 +0200

    .

--- src/jpu2016/dogfight/model/Mobile.java
+++ src/jpu2016/dogfight/model/Mobile.java
@@ -1,5 +1,69 @@
 package jpu2016.dogfight.model;
 
 public class Mobile {
+	private int speed;
 	
+	public Mobile(Direction direction, Position position, Dimension dimension, int speed, String image) {
+		
+	}
+	public Direction getDirection() {
+		
+	}
+	public void setDirection(Direction direction) {
+		
+	}
+	public Dimension getDimension() {
+		
+	}
+	public void setDimension(Dimension dimension) {
+		
+	}
+	public int getSpeed() {
+		
+	}
+	public int getWidth() {
+		
+	}
+	public int getHeight() {
+		
+	}
+	public void move() {
+		
+	}
+	public void placeInArea(IArea area) {
+		
+	}
+	public boolean isPlayer(int player) {
+		
+	}
+	private void moveUp() {
+		
+	}
+	private void moveRight() {
+		
+	}
+	private void moveDown() {
+		
+	}
+	private void moveLeft() {
+		
+	}
+	public Color getColor() {
+		
+	}
+	public IDogfightModel getDogfightModel() {
+		
+	}
+	public void setDogfightModel(IdogfightModel dogfightModel) {
+		
+	}
+	public boolean hit() {
+		
+	}
+	public boolean isWeapon() {
+		
+	}
+	public Image getImage() {
+		
+	}
 }
--- src/jpu2016/dogfight/model/Sky.java
+++ src/jpu2016/dogfight/model/Sky.java
@@ -1,6 +1,13 @@
 package jpu2016.dogfight.model;
 
 public class Sky {
-
-	
+	public Sky(Dimension dimension) {
+		
+	}
+	public Dimension getDimension() {
+		
+	}
+	public Image getImage() {
+		
+	}
 }
