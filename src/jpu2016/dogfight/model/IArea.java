1d5a0c085f261eaadacb72debb13aa34473f66fd
julienliguori <33621048+julienliguori@users.noreply.github.com>
2018-04-24 10:55:06 +0200

    partie 3

--- src/jpu2016/dogfight/model/IArea.java
+++ src/jpu2016/dogfight/model/IArea.java
@@ -1,6 +1,10 @@
 package jpu2016.dogfight.model;
 
 public class IArea {
-
-	
+	public Dimension getDimension() {
+		
+	}
+	public Image getImage() {
+		
+	}
 }
--- src/jpu2016/dogfight/model/IDogfightModel.java
+++ src/jpu2016/dogfight/model/IDogfightModel.java
@@ -1,6 +1,25 @@
 package jpu2016.dogfight.model;
 
 public class IDogfightModel {
-
-	
+	public IArea getArea() {
+		
+	}
+	public void buildArea(Dimension dimension) {
+		
+	}
+	public void addMobile(IMobile Mobile) {
+		
+	}
+	public void removeMobile(IMobile Mobile) {
+		
+	}
+	public ArrayList<IMobil> getMobiles(){
+		
+	}
+	public IMobile getMobileByPlayer(int player){
+		
+	}
+	public void setMobilesHabesMoved() {
+		
+	}
 }
